5825af81ec65c6ff259b637a09940cd5a07fb2ec
alirzaMhd <71058261+alirzaMhd@users.noreply.github.com>
2023-09-05 20:03:39 +0430

    fized main.dart

--- lib/constants/ui_constants.dart
+++ lib/constants/ui_constants.dart
@@ -1,4 +1,6 @@
 class UIConstants {
+  static const double screenWidth = 375;
+  static const double screenHeight = 812;
   static const double mainButtonCornerRoundness = 6;
   static const double mainButtonWidth = 343;
   static const double mainButtonHeight = 64;
